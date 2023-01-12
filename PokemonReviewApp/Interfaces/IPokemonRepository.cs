db6d9d132648b399a958b48e1e0d83fc00ae0924
Sofia <Sofia@DESKTOP-27NEB8F>
2023-01-12 08:08:35 +0300

    Add project files.

---
+++ PokemonReviewApp/Interfaces/IPokemonRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IPokemonRepository
+    {
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        ICollection<Pokemon> GetPokemons();
+        decimal GetPokemonRating(int id);
+        bool PokemonExist(int pokeId);
+    }
+}
---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,40 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemons.Where(p => p.Id == id).FirstOrDefault();
+        }
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemons.Where(p => p.Name == name).FirstOrDefault();
+        }
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+        public decimal GetPokemonRating(int id)
+        {
+            var review = _context.Reviews.Where(p => p.Pokemon.Id == id);
+
+            if (review.Count() <= 0)
+                return 0;
+
+            return ((decimal)review.Sum(r => r.Rating) / review.Count());
+        }
+        public bool PokemonExist(int pokeId)
+        {
+            return _context.Pokemons.Any(p => p.Id == pokeId);
+        }
+    }
+}
